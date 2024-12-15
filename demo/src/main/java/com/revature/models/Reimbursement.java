6d8ff1d02c66e544e3b7338eea9d768a42267d06
John Xu <johnxu0225@gmail.com>
2024-12-15 06:51:14 -0500

    modeling reimbursement

--- demo/src/main/java/com/revature/models/Reimbursement.java
+++ demo/src/main/java/com/revature/models/Reimbursement.java
@@ -1,4 +1,12 @@
 package com.revature.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import org.springframework.stereotype.Component;
+
+@Component //make class a bean
+@Entity //makes a DB based table on this class
+@Table(name = "reimbursements")
+
 public class Reimbursement {
 }
