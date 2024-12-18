1c61e40950594e38df1e2a2d05cc826d9d0be527
John Xu <johnxu0225@gmail.com>
2024-12-18 17:26:09 -0500

    in progress

---
+++ demo/src/main/java/com/revature/controllers/UserController.java
@@ -0,0 +1,29 @@
+package com.revature.controllers;
+
+import com.revature.models.DTOs.IncomingUserDTO;
+import com.revature.models.User;
+import com.revature.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    //Inserting user
+    @PostMapping
+    public ResponseEntity<User> insertUser(@RequestBody IncomingUserDTO user){
+
+    }
+}
---
+++ demo/src/main/java/com/revature/models/DTOs/IncomingUserDTO.java
@@ -0,0 +1,88 @@
+package com.revature.models.DTOs;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class IncomingUserDTO {
+
+    private int userId;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String role;
+
+    public IncomingUserDTO() {
+    }
+
+    public IncomingUserDTO(int userId, String firstName, String lastName, String username, String password, String role) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "IncomingUserDTO{" +
+                "userId=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
